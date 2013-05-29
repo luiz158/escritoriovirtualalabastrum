1011241e15058e4f634a0d36d5ead8a746383df2
Renan de Andrade Vaz <renanandrade_rj@hotmail.com>
2013-05-28 22:59:29 -0300

    -

--- src/escritoriovirtualalabastrum/controller/ImportacaoArquivoController.java
+++ src/escritoriovirtualalabastrum/controller/ImportacaoArquivoController.java
@@ -1,8 +1,13 @@
 package escritoriovirtualalabastrum.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import au.com.bytecode.opencsv.CSVReader;
 import br.com.caelum.vraptor.Resource;
@@ -12,6 +17,9 @@ import escritoriovirtualalabastrum.anotacoes.Funcionalidade;
 
 @Resource
 public class ImportacaoArquivoController {
+
+	private static final String PASTA_PADRAO_WINDOWS = "C:\\csvExtraido";
+	private static final String PASTA_PADRAO_LINUX = "/csvExtraido";
 
 	private Result result;
 
@@ -28,15 +36,70 @@ public class ImportacaoArquivoController {
 	@Funcionalidade(administrativa = "true")
 	public void importarArquivoTabelaRelacionamentos(UploadedFile arquivo) throws IOException {
 
+		String caminho = null;
+
+		if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+
+			caminho = PASTA_PADRAO_WINDOWS;
+		}
+
+		else {
+
+			caminho = PASTA_PADRAO_LINUX;
+		}
+
 		InputStream file = arquivo.getFile();
 
-		@SuppressWarnings("resource")
-		CSVReader reader = new CSVReader(new InputStreamReader(file));
+		if (arquivo.getFileName().endsWith(".zip")) {
 
-		String[] nextLine;
-		while ((nextLine = reader.readNext()) != null) {
+			byte[] buffer = new byte[1024];
 
-			System.out.println(nextLine[0]);
+			ZipInputStream zis = new ZipInputStream(file);
+
+			ZipEntry ze = zis.getNextEntry();
+
+			while (ze != null) {
+
+				String fileName = ze.getName();
+
+				File newFile = new File(caminho + File.separator + fileName);
+
+				new File(newFile.getParent()).mkdirs();
+
+				FileOutputStream fos = new FileOutputStream(newFile);
+
+				int len;
+				while ((len = zis.read(buffer)) > 0) {
+					fos.write(buffer, 0, len);
+				}
+
+				fos.close();
+				ze = zis.getNextEntry();
+			}
+
+			zis.closeEntry();
+			zis.close();
+
+			@SuppressWarnings("resource")
+			CSVReader reader = new CSVReader(new FileReader(new File(caminho + File.separator + arquivo.getFileName().split(".zip")[0] + ".csv")));
+
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+
+				System.out.println(nextLine[0]);
+			}
+		}
+
+		else if (arquivo.getFileName().endsWith(".csv")) {
+
+			@SuppressWarnings("resource")
+			CSVReader reader = new CSVReader(new InputStreamReader(file));
+
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+
+				System.out.println(nextLine[0]);
+			}
 		}
 
 		arquivo.getFile().close();
