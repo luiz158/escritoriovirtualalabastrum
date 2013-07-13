1067f14b852885056f94f7d54d74bea40064df4b
Renan de Andrade Vaz <renanandrade_rj@hotmail.com>
2013-07-12 22:04:07 -0300

    -

--- src/escritoriovirtualalabastrum/controller/LoginController.java
+++ src/escritoriovirtualalabastrum/controller/LoginController.java
@@ -104,8 +104,7 @@ public class LoginController {
 					if (Util.vazio(usuarioBanco.getEV()) || usuarioBanco.getEV().equals("0")) {
 
 						String mensagem = "O usuário " + usuarioBanco.getId_Codigo() + " - " + usuarioBanco.getvNome() + " tentou acessar o EV mas o acesso está bloqueado para ele.";
-						// JavaMailApp.enviarEmail("Código não habilitado para acessar o escritório virtual",
-						// "suporte@alabastrum.com.br", mensagem);
+						JavaMailApp.enviarEmail("Código não habilitado para acessar o escritório virtual", "suporte@alabastrum.com.br", mensagem);
 
 						validator.add(new ValidationMessage("Código não habilitado para acessar o escritório virtual. Para poder acessar o escritório virtual, você deve antes ficar ativo. Você pode ficar ativo fazendo uma compra de produtos da Alabastrum através desta tela.", "Atenção"));
 						this.sessaoGeral.adicionar("codigoUsuarioRealizandoPedido", usuarioBanco.getId_Codigo());
