83b504f54d987b9baef9f775bf9d479487c3036c
renandeandradevaz <renanandrade_rj@hotmail.com>
2014-03-29 09:54:13 -0300

    default commit description

--- src/escritoriovirtualalabastrum/controller/LoginController.java
+++ src/escritoriovirtualalabastrum/controller/LoginController.java
@@ -279,7 +279,15 @@ public class LoginController {
 
 			if (Util.vazio(usuarioBanco.getCadAtividade()) || usuarioBanco.getCadAtividade().equals("0")) {
 
-				validator.add(new ValidationMessage("Você atualmente está inativo e não pode acessar o escritório virtual. Para poder acessar o escritório virtual, você deve antes ficar ativo. Você pode ficar ativo fazendo uma compra de produtos da Alabastrum através desta tela.", "Atenção"));
+				// MOMENTANEAMENTE
+				colocarUsuarioNaSessao(usuarioBanco);
+
+				// validator.add(new
+				// ValidationMessage("Você atualmente está inativo e não pode acessar o escritório virtual. Para poder acessar o escritório virtual, você deve antes ficar ativo. Você pode ficar ativo fazendo uma compra de produtos da Alabastrum através desta tela.",
+				// "Atenção"));
+
+				validator.add(new ValidationMessage("Você atualmente está inativo e para acessar algumas funcionalidades do EV precisa antes ficar ativo. Você pode ficar ativo fazendo uma compra de produtos da Alabastrum através desta tela.", "Atenção"));
+
 				this.sessaoGeral.adicionar("codigoUsuarioRealizandoPedido", usuarioBanco.getId_Codigo());
 				validator.onErrorRedirectTo(PedidoController.class).acessarTelaPedido();
 			}
